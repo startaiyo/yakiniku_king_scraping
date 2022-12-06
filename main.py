82795a4bd883cbd5b777f6df79995f32d81b7bf1
startaiyo <startaiyo0104@gmail.com>
2022-12-06 10:17:11 +0900

    日付浸ける

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import datetime
 
 # use creds to create a client to interact with the Google Drive API
 scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -13,11 +14,11 @@ client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-sheet = client.open("焼肉キング戦いの記録")."2022/11/17"
+book = client.open("焼肉キング戦いの記録")
+sheet = book.worksheet('2022/12/3')
 
 # Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+list_of_hashes = sheet.get_all_values()
 
 def split_list(l, n):
     """
@@ -29,8 +30,11 @@ def split_list(l, n):
     for idx in range(0, len(l), n):
         yield l[idx:idx + n]
 
-def paste_to_spreadsheet(paste_list):
-
+def add_record_to_new_sheet(record_list):
+    DIFF_JST_FROM_UTC = 9
+    now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+    now_for_title = now.strftime('%Y/%m/%d')
+    book.add_worksheet(now_for_title, rows = 100, cols = 4)
 
 response_for_tanpin_tag = requests.get("https://www.yakiniku-king.jp/menu_all/tanpin/")
 all_tanpin_tag = BeautifulSoup(response_for_tanpin_tag.content, 'html.parser').select('a[href*="menu_all/tanpin"]')
@@ -44,9 +48,9 @@ tanpin_tag_href_list = list(filter(lambda x: not x.replace("/","").endswith("tan
 #     soup = BeautifulSoup(r.content, 'html.parser')
 #     item_name_list.extend(list(split_list(list(map(lambda x: x.text, soup.select("div.item_description h2, div.item_description span"))),3)))
 # print(item_name_list)
-response_for_kikan_gentei = requests.get("https://www.yakiniku-king.jp/menu_all/season/")
-all_kikan_gentei = BeautifulSoup(response_for_kikan_gentei.content, 'html.parser').select('a[href*="menu_all/season"]')
-all_href_list_kikan_gentei = list(set(list(map(lambda x: "https://www.yakiniku-king.jp" + x.get('href'), all_kikan_gentei))))
-kikan_gentei_tag_href_list = list(filter(lambda x: not x.replace("/","").endswith("season"), all_href_list_kikan_gentei))
-print(kikan_gentei_tag_href_list)
+# response_for_kikan_gentei = requests.get("https://www.yakiniku-king.jp/menu_all/season/")
+# all_kikan_gentei = BeautifulSoup(response_for_kikan_gentei.content, 'html.parser').select('a[href*="menu_all/season"]')
+# all_href_list_kikan_gentei = list(set(list(map(lambda x: "https://www.yakiniku-king.jp" + x.get('href'), all_kikan_gentei))))
+# kikan_gentei_tag_href_list = list(filter(lambda x: not x.replace("/","").endswith("season"), all_href_list_kikan_gentei))
+# print(kikan_gentei_tag_href_list)
 
