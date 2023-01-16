59acbc200d283f364347a0b8e268c95799a8507f
startaiyo <startaiyo0104@gmail.com>
2023-01-16 09:48:42 +0900

    登録フォームを作成

--- app/apis/main.py
+++ app/apis/main.py
@@ -6,22 +6,24 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 
-api = Blueprint('main', __name__, url_prefix = '/api/register/')
+api = Blueprint('main', __name__, url_prefix = '/yakiniku_king')
 
 # use creds to create a client to interact with the Google Drive API
-# scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secrets.json', scope)
-# client = gspread.authorize(creds)
+scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('app/apis/client_secret.json', scope)
+client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
-# book = client.open("焼肉キング戦いの記録")
-# sheet = book.worksheet('2022/12/3')
+book = client.open("焼肉キング戦いの記録")
+sheet = book.worksheet('2022/12/3')
 
 # Extract and print all of the values
 # list_of_hashes = sheet.get_all_values()
+
+data = [['北海道夢の大地豚壺漬け豚ジンギスカン', 690, 759, '期間限定'], ['旭川発祥一本塩ホルモン※数量限定', 390, 429, '期間限定'], ['紅ずわいのコールスロー※数量限定', 390, 429, '期間限定']]
 
 def split_list(l, n):
     """
@@ -33,11 +35,11 @@ def split_list(l, n):
     for idx in range(0, len(l), n):
         yield l[idx:idx + n]
 
-# def add_price_to_sheet(item_list):
-#     price_sheet = book.worksheet('値段リスト')
-#     price_sheet.clear()
-#     item_list = [["品名", "本体価格", "税込み価格"]] + item_list
-#     price_sheet.append_rows(item_list)
+def add_price_to_sheet(item_list):
+    price_sheet = book.worksheet('値段リスト')
+    price_sheet.clear()
+    item_list = [["品名", "本体価格", "税込み価格", "ジャンル"]] + item_list
+    price_sheet.append_rows(item_list)
 
 # def add_record_to_new_sheet(record_list):
 #     DIFF_JST_FROM_UTC = 9
@@ -50,10 +52,24 @@ def make_num(price_info):
     price_info[2] = int(price_info[2].replace("円)", "").replace("(税込", "").replace(",", ""))
     return price_info
 
-# add_price_to_sheet(item_name_list)
-
 @api.route('')
-def get_all():
+def render_page():
+    return render_template('index.html')
+
+@api.route('/api/register', methods = ['GET', 'POST'])
+def show_menu():
+    if request.method == 'POST':
+        result_sets = []
+        for key, value in request.form.items():
+            if value != "":
+                result_sets.append(key.split(",") + [value])
+        print(result_sets)
+    menu_sheet = book.worksheet('値段リスト')
+    menu_list = menu_sheet.get_all_values()
+    return render_template('index.html', data = menu_list)
+
+@api.route('/api/renew')
+def renew_all():
     response_for_tanpin_tag = requests.get("https://www.yakiniku-king.jp/menu_all/tanpin/")
     all_tanpin_tag = BeautifulSoup(response_for_tanpin_tag.content, 'html.parser').select('a[href*="menu_all/tanpin"]')
     all_href_list = list(map(lambda x: "https://www.yakiniku-king.jp" + x.get('href'), all_tanpin_tag))
@@ -65,7 +81,12 @@ def get_all():
         soup = BeautifulSoup(r.content, 'html.parser')
         for tag in soup.select("div.item_description h2 span"):
             tag.decompose()
-        item_name_list.extend(list(split_list(list(map(lambda x: "".join(x.text.split()), soup.select("div.item_description h2, div.item_description span"))),3)))
+        menu_title = soup.select("h1.pageTitle")[0].text
+        # [menu, price, tax-in price]
+        item_and_price_list = list(split_list(list(map(lambda x: "".join(x.text.split()), soup.select("div.item_description h2, div.item_description span"))),3))
+        # [menu, price, tax-in price, title]
+        item_and_menu_title_and_price_list = list(map(lambda x: x + [menu_title], item_and_price_list))
+        item_name_list.extend(item_and_menu_title_and_price_list)
     response_for_kikan_gentei = requests.get("https://www.yakiniku-king.jp/menu_all/season/")
     all_kikan_gentei = BeautifulSoup(response_for_kikan_gentei.content, 'html.parser').select('a[href*="menu_all/season"]')
     all_href_list_kikan_gentei = list(set(list(map(lambda x: "https://www.yakiniku-king.jp" + x.get('href'), all_kikan_gentei))))
@@ -74,6 +95,7 @@ def get_all():
         time.sleep(0.5)
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        item_name_list.append(list(map(lambda x: "".join(x.text.split()), soup.select("div.menuSelectTtl, div.price span"))))
+        item_name_list.append(list(map(lambda x: "".join(x.text.split()), soup.select("div.menuSelectTtl, div.price span"))) + ["期間限定"])
     item_name_list_using_num = list(map(make_num, item_name_list))
-    return jsonify(item_name_list_using_num), 200
+    add_price_to_sheet(item_name_list_using_num)
+    return jsonify({"text": "最新の状態です！！"}), 200
--- app/server.py
+++ app/server.py
@@ -3,4 +3,5 @@ from flask import Flask
 from app.apis import main
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(main.api)
